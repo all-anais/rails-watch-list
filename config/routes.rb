3cd2c6d36ba21a969105a00e44ade980fa5320b0
Anais ALLALI <anais.allali@kedgebs.com>
2024-08-09 11:49:22 +0200

    exo

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :lists, only: %i[index create show new] do
+    resources :bookmarks, only: %i[create new]
+  end
+  resources :bookmarks, only: %i[destroy]
+  root 'lists#index'
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
